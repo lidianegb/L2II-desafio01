6b0984107e9025cd180674fd845cae7793c892af
lidianegb <lidianegomesbarbosa@gmail.com>
2021-09-17 10:39:46 -0300

    arquivos de teste removido

--- desafioLp2/src/main/java/com/lidiane/desafioLp2/DesafioLp2Application.java
+++ desafioLp2/src/main/java/com/lidiane/desafioLp2/DesafioLp2Application.java
@@ -1,12 +1,7 @@
 package com.lidiane.desafioLp2;
 
-import com.lidiane.desafioLp2.model.entities.Client;
-import com.lidiane.desafioLp2.model.entities.ClientMock;
-import com.lidiane.desafioLp2.model.services.ClientService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DesafioLp2Application {
